81abc03cd1c33f9713aae1703f7274ac9b8c0fbb
bwatson <bwatson@ncsu.edu>
2017-10-04 00:40:42 -0400

    debug last translate

--- rasterize.js
+++ rasterize.js
@@ -187,6 +187,7 @@ function renderTriangles() {
     mat4.multiply(inputTriangles[0].mMatrix,
                   mat4.fromRotation(mat4.create(),Math.PI/2,vec3.fromValues(0,0,1)),
                   inputTriangles[0].mMatrix); // rotate 90 degs
+    console.log(str(mat4.fromTranslation(vec3.create(),setCenter)));
     mat4.multiply(inputTriangles[0].mMatrix,
                   mat4.fromTranslation(vec3.create(),setCenter),
                   inputTriangles[0].mMatrix); // move back to center
